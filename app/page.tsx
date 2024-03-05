fc272707c1f9a495246383b53cfb522f27fb5da0
Michaela <mi-and@outlook.com>
2024-03-05 15:23:41 +0100

    Map out all mocked data

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import { mockedProduct } from "./lib/mockedProduct";
+
 export default function Home() {
   return (
     <main>
       <p>Det här är startsidan. Här ska alla produkterna visas.</p>
+
+      {/* Här mappas en produkt ut. Detta blir ett produkt kort sen. */}
+      {mockedProduct.map((product) => (
+        <div>
+          <img src={product.image} alt="testProduct" />
+          <p>{product.title}</p>
+          <p>{product.price}</p>
+          <p>{product.content}</p>
+        </div>
+      ))}
     </main>
   );
 }
