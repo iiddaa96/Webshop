5a16b614a2840dc0c4d4ea7a2117f1556beda916
Michaela <mi-and@outlook.com>
2024-03-06 09:08:15 +0100

    Create routing, NOT done

---
+++ app/admin/[slug]/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+function AddNewProduct() {
+  return (
+    <div>
+      <h1>AddNewProduct Page</h1>
+      <Link href="/admin">
+        <button>Spara/Tillbaka</button>
+      </Link>
+    </div>
+  );
+}
+
+export default AddNewProduct;
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 function Admin() {
   return (
     <div>
       <h1>Admin sidan</h1>
+
+      <Link href="/admin/updateExistProduct">
+        <button>Uppdatera produkt</button>
+      </Link>
+      <Link href="/admin/AddNewProduct">
+        <button>Lägg till ny</button>
+      </Link>
     </div>
   );
 }
