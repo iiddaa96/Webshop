3e558fc209e1fe2de8ae9df6208a10649bb02d9d
Michaela <mi-and@outlook.com>
2024-03-08 16:14:51 +0100

    Style new product page

--- app/admin/product/new/page.tsx
+++ app/admin/product/new/page.tsx
@@ -1,4 +1,16 @@
-import Link from "next/link";
+"use client";
+
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import SaveIcon from "@mui/icons-material/Save";
+import {
+  Box,
+  Button,
+  Container,
+  Link,
+  TextField,
+  Typography,
+  styled,
+} from "@mui/material";
 
 /* CYPRESS TESTER SOM SKA FINNAS MED  */
 /* - `data-cy="product"` produkt-korten/raden på startsidan & adminsidan. 
@@ -14,14 +26,94 @@ import Link from "next/link";
 - `data-cy="product-image-error"` felmeddelande vid felaktigt angiven bild.
 */
 
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
+
 function AddNewProduct() {
   return (
-    <div>
-      <h1>AddNewProduct Page</h1>
-      <Link href="/admin/product">
-        <button>Spara/Tillbaka</button>
-      </Link>
-    </div>
+    <Container
+      fixed
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: "30px",
+        marginBottom: "30px",
+      }}
+    >
+      <Box
+        sx={{
+          height: 700,
+          borderRadius: "10px",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "20px",
+          boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.3)",
+        }}
+      >
+        <Typography>Här kommer en bild</Typography>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-start",
+            marginBottom: "20px",
+          }}
+        >
+          <Button
+            component="label"
+            role={undefined}
+            variant="contained"
+            tabIndex={-1}
+            startIcon={<CloudUploadIcon />}
+          >
+            Upload file
+            <VisuallyHiddenInput type="file" />
+          </Button>
+        </Box>
+        <TextField
+          fullWidth
+          label="Title"
+          helperText=" "
+          id="demo-helper-text-aligned-no-helper"
+          sx={{ width: "100%", marginBottom: "20px" }}
+        />
+        <TextField
+          fullWidth
+          label="Price"
+          helperText=" "
+          id="demo-helper-text-aligned-no-helper"
+          sx={{ width: "100%", marginBottom: "20px" }}
+        />
+
+        <TextField
+          id="outlined-multiline-static"
+          label="Description"
+          multiline
+          rows={6}
+          variant="outlined"
+          sx={{ width: "100%", marginBottom: "20px" }}
+        />
+        <Box sx={{ display: "flex", gap: "5vh" }}>
+          <Link href="/admin/product">
+            <Box component={"button"} sx={{ width: "150px" }}>
+              <SaveIcon fontSize="large" />
+            </Box>
+          </Link>
+        </Box>
+      </Box>
+    </Container>
   );
 }
 
