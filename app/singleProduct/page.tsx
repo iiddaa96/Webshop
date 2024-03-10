ef776e348bcd1e35a09c99a24366d1004407c8e8
maryamakeyf <maryamosalad98@outlook.com>
2024-03-10 13:26:59 +0100

    added grid system

--- app/singleProduct/page.tsx
+++ app/singleProduct/page.tsx
@@ -10,54 +10,56 @@
 
 
 
-
-
 import Link from "next/link";
 import { products } from "../../data/index";
 import { Box, Button, Grid, IconButton, Typography } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 
-function singleProduct() {
-  // Check if products array is not empty
- 
-    return (
-      <div>
-      <h2>Single product page</h2>
-  {/* renderar single product card */}
-  <Box sx={{ flexGrow: 1 }}>
-  {/* Render only the first product */}
-  {products.length > 0 && (
-    <div key={products[0].id}>
-      <img src={products[0].image} alt="testProduct" />
-      <p>{products[0].title}</p>
-      <p>{products[0].price}</p>
-      <p>{products[0].description}</p>
-    </div>
-  )}
-</Box>
+function SingleProduct() {
 
-   {/* knapp till checkout sidan 
+ 
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <h2>Single product pageee</h2>
+      </Grid>
+      <Grid item xs={12}>
+        {/* Render single product card */}
+        <Box sx={{ flexGrow: 1 }}>
+          {/* Render only the first product */}
+          {products.length > 0 && (
+            <div key={products[0].id}>
+              <img src={products[0].image} alt="testProduct" />
+              <p>{products[0].title}</p>
+              <p>{products[0].price}</p>
+              <p>{products[0].description}</p>
+            </div>
+          )}
+        </Box>
+      </Grid>
+      <Grid item xs={12}>
+        {/* Button to checkout page */}
         <Button component={Link} href="/checkout" color="inherit">
           Posters
-        </Button>*/}
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
-      <IconButton color="primary" aria-label="remove from cart">
-        <RemoveIcon />
-      </IconButton>
-      <Button variant="contained" color="primary">
-        <Typography  component="span">1</Typography>
-      </Button>
-      <IconButton color="primary" aria-label="add to cart">
-        <AddIcon />
-      </IconButton>
-    </Box>
-  
-  </div>
-    )
-      
-     
-  }
+        </Button>
+      </Grid>
+      <Grid item xs={12}>
+        {/* Add to Cart functionality */}
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <IconButton color="primary" aria-label="remove from cart">
+            <RemoveIcon />
+          </IconButton>
+          <Button variant="contained" color="primary">
+            <Typography component="span">1</Typography>
+          </Button>
+          <IconButton color="primary" aria-label="add to cart">
+            <AddIcon />
+          </IconButton>
+        </Box>
+      </Grid>
+    </Grid>
+  );
+}
 
-
-export default singleProduct;
+export default SingleProduct;
