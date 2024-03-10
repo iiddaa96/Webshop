2cd145c54a6d6643636a7af65636ce4bc08ff1f6
maryamakeyf <maryamosalad98@outlook.com>
2024-03-10 10:32:58 +0100

    add single product to page

---
+++ app/singleProduct/page.tsx
@@ -0,0 +1,54 @@
+/* 
+- `data-cy="product"` produkt-korten/raden på startsidan & adminsidan.
+- `data-cy="product-id"` id på en produkt.
+- `data-cy="product-title"` titeln på en produkt.
+- `data-cy="product-price"` priset på en produkt.
+- `data-cy="product-description"` beskrivningen av en produkt.
+- `data-cy="product-buy-button"` lägg till i kundvagnen knappen.
+- `data-cy="added-to-cart-toast"` toast som visas när en produkt läggs till i kundvagnen.
+*/
+
+
+
+
+
+import Link from "next/link";
+import { products } from "../../data/index";
+
+import { Box, Typography} from "@mui/material";
+
+
+
+function singleProduct() {
+  // Check if products array is not empty
+ 
+    return (
+      <div>
+      <h2>Single product page</h2>
+  {/* renderar single product card */}
+  <Box sx={{ flexGrow: 1 }}>
+  {/* Render only the first product */}
+  {products.length > 0 && (
+    <div key={products[0].id}>
+      <img src={products[0].image} alt="testProduct" />
+      <p>{products[0].title}</p>
+      <p>{products[0].price}</p>
+      <p>{products[0].description}</p>
+    </div>
+  )}
+</Box>
+
+   {/* knapp till checkout sidan 
+        <Button component={Link} href="/checkout" color="inherit">
+          Posters
+        </Button>*/}
+
+  
+  </div>
+    )
+      
+     
+  }
+
+
+export default singleProduct;
