483230e08f777ef8deeddebbef584ba2a698629b
maryamakeyf <maryamosalad98@outlook.com>
2024-03-13 11:23:17 +0100

    add button to component ui

--- app/singleProduct/page.tsx
+++ app/singleProduct/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+
 /* 
 - `data-cy="product"` produkt-korten/raden på startsidan & adminsidan.
 - `data-cy="product-id"` id på en produkt.
@@ -8,33 +8,15 @@
 - `data-cy="product-buy-button"` lägg till i kundvagnen knappen.
 - `data-cy="added-to-cart-toast"` toast som visas när en produkt läggs till i kundvagnen.
 */
-import React, { useState } from 'react';
+
 import { products } from "../../data/index";
-import { Box, Button, Grid, IconButton, Typography } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
-import RemoveIcon from '@mui/icons-material/Remove';
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Box,  Grid, Typography } from '@mui/material';
+import QuantityButton from "../ui/quantityButton";
+
 
 
 export default function SingleProduct() {
-  // Definerar en base price
-  const basePrice: number = products.length > 0 ? products[0].price : 0;
-  const [quantity, setQuantity] = useState(1);
-// function för increment
-  const incrementQuantity = () => {
-    setQuantity(quantity + 1);
-  };
-// function för decrement
-  const decrementQuantity = () => {
-    if (quantity > 0) {
-      setQuantity(quantity - 1);
-    }
-  };
 
-  // Function to calculate the total price based on quantity
-  const calculateTotalPrice = (): number => {
-    return basePrice * quantity;
-  };
 
   return (
     <Grid container spacing={2}>
@@ -57,45 +39,16 @@ export default function SingleProduct() {
             <div key={products[0].id}>
               {/* gutterbottom lägger till bottom margin*/}
               <Typography variant="h4" gutterBottom>{products[0].title}</Typography>
-              <Typography variant="body1" gutterBottom>Price: {basePrice}kr</Typography>
+         
               <Typography variant="body2" gutterBottom>{products[0].description}</Typography>
             </div>
           )}
         </Box>
 
-        {/* Add to Cart functionality */}
-        <Box>
-          <Box sx={{ display: 'flex', alignItems: 'center', marginLeft: '2px' }}>
-            <IconButton color="primary" aria-label="decrement" onClick={decrementQuantity}>
-              <RemoveIcon />
-            </IconButton>
-            <Button variant="contained" color="primary">
-              <Typography component="span">{quantity}</Typography>
-            </Button>
-            <IconButton color="primary" aria-label="increment" onClick={incrementQuantity}>
-              <AddIcon />
-            </IconButton>
-          </Box>
-
-          {/* Displays total price på sidan */}
-          <Box>
-            <Typography variant="h6">Total Price: {calculateTotalPrice()}kr</Typography>
-          </Box>
-
-          {/* Add to cart button */}
-          <Box sx={{ display: 'flex', alignItems: 'center', height: '100px' }}>
-            <button style={{
-              borderRadius: '5px',
-              cursor: 'pointer',
-              fontWeight: 'bold',
-              boxShadow: '0 2px 4px rgba(0,0,0,0.2)',
-              transition: 'background-color 0.3s',
-              margin: '25px',
-              height: '50%',
-              width: '100%',
-            }}>Add to cart <ShoppingCartIcon /> </button>
-          </Box>
-        </Box>
+    
+        <QuantityButton />
+    
+        
       </Grid>
     </Grid>
   );
