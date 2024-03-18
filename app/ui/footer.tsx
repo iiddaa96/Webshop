dce4758495fd2022e26193133013976955063eed
Michaela <mi-and@outlook.com>
2024-03-18 11:08:31 +0100

    Style footer, desktop and mobile

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -4,50 +4,76 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import ContactPhoneIcon from "@mui/icons-material/ContactPhone";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import SearchIcon from "@mui/icons-material/Search";
-import { Box, IconButton, Link, Typography } from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import { Box, Typography } from "@mui/material";
 import { useState } from "react";
-
 function Footer() {
-  const [selected, setSelected] = useState("contact"); 
+  const [selected, setSelected] = useState("contact");
 
   return (
     <Box
       component="footer"
       sx={{
-        border: "1px solid black",
+        borderTop: "1px solid black",
+        justifyContent: "space-around",
         display: "flex",
-        justifyContent: "space-around", 
         alignItems: "center",
-        padding: "10px 0", 
-        backgroundColor: "#f0f0f0", 
+        padding: "10px 0",
+        backgroundColor: "white",
+        flexDirection: "row", // För desktop-läge (standard)
+        "@media (max-width: 600px)": {
+          flexDirection: "column", // För mobilläge
+          padding: "20px",
+        },
       }}
     >
-      <IconButton
-        component={Link}
-        href="/confirmation"
-        onClick={() => setSelected("contact")}
-        color={selected === "contact" ? "primary" : "default"}
+      <Box
+        sx={{
+          "@media (max-width: 600px)": {
+            paddingBottom: "15px",
+          },
+        }}
       >
-        <ContactPhoneIcon />
-        <Typography>Contact</Typography>
-      </IconButton>
-      <IconButton
-        onClick={() => setSelected("favorites")}
-        color={selected === "favorites" ? "primary" : "default"}
+        <Typography sx={{ fontSize: "14px", fontWeight: "bold" }}>
+          Kontakta oss:
+        </Typography>
+        <Typography sx={{ fontSize: "14px" }}>Wall of Art </Typography>
+        <Typography sx={{ fontSize: "14px" }}>Hittepå gatan 01</Typography>
+        <Typography sx={{ fontSize: "14px" }}>442 11 Göteborg</Typography>
+      </Box>
+
+      <Box
+        sx={{
+          "@media (max-width: 600px)": {
+            paddingBottom: "15px",
+          },
+        }}
       >
-        <FavoriteIcon />
-        <Typography>Favorites</Typography>
-      </IconButton>
-      <IconButton
-        onClick={() => setSelected("search")}
-        color={selected === "search" ? "primary" : "default"}
+        <Typography sx={{ textAlign: "center", fontSize: "14px" }}>
+          ©️ Copyright by
+        </Typography>
+        <Typography sx={{ fontSize: "14px" }}>Wall of art company</Typography>
+        <Typography sx={{ fontSize: "14px" }}>Team JIMM 2024</Typography>
+      </Box>
+
+      <Box
+        sx={{
+          "@media (max-width: 600px)": {
+            paddingBottom: "15px",
+          },
+        }}
       >
-        <SearchIcon />
-        <Typography>Nearby</Typography>
-      </IconButton>
+        <Typography
+          sx={{ textAlign: "center", fontSize: "14px", fontWeight: "bold" }}
+        >
+          Följ oss
+        </Typography>
+        <FacebookIcon sx={{ fontSize: "2rem" }}></FacebookIcon>
+        <InstagramIcon sx={{ fontSize: "2rem" }}></InstagramIcon>
+        <YouTubeIcon sx={{ fontSize: "2rem" }}></YouTubeIcon>
+      </Box>
     </Box>
   );
 }
