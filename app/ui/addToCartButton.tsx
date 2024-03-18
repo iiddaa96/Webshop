469d6849aa328d49deed59e52d8226e7dfe81f69
Ida Casperson <ida_casperson@hotmail.com>
2024-03-18 12:44:02 +0100

    fixing onClick function. Fixing two functions in one onClick

--- app/ui/addToCartButton.tsx
+++ app/ui/addToCartButton.tsx
@@ -3,6 +3,7 @@ import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import { IconButton } from "@mui/material";
 import React from "react";
 import { Product } from "../../data/index";
+import { useCart } from "../context/CartContext";
 
 interface AddToCartButtonProps {
   product: Product;
@@ -15,11 +16,18 @@ const AddToCartButton: React.FC<AddToCartButtonProps> = ({
   product,
   handleAddToCart,
 }) => {
+  const { addToCart } = useCart();
+
   return (
     <IconButton
       aria-label="add to cart"
       // Anropa handleAddToCart-funktionen med produkttiteln
-      onClick={() => handleAddToCart(product)}
+      // onClick={() => handleAddToCart(product)}
+      // onClick={() => addToCart(product)}
+      onClick={() => {
+        handleAddToCart(product);
+        addToCart(product);
+      }}
     >
       <AddShoppingCartIcon />
     </IconButton>
@@ -27,3 +35,33 @@ const AddToCartButton: React.FC<AddToCartButtonProps> = ({
 };
 
 export default AddToCartButton;
+
+// "use client";
+// import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+// import { IconButton } from "@mui/material";
+// import React from "react";
+// import { Product } from "../../data/index";
+
+// interface AddToCartButtonProps {
+//   product: Product;
+//   title: string;
+//   // Lägg till funktion för att hantera "Lägg till i kundvagnen"
+//   handleAddToCart: (products: Product) => void;
+// }
+
+// const AddToCartButton: React.FC<AddToCartButtonProps> = ({
+//   product,
+//   handleAddToCart,
+// }) => {
+//   return (
+//     <IconButton
+//       aria-label="add to cart"
+//       // Anropa handleAddToCart-funktionen med produkttiteln
+//       onClick={() => handleAddToCart(product)}
+//     >
+//       <AddShoppingCartIcon />
+//     </IconButton>
+//   );
+// };
+
+// export default AddToCartButton;
