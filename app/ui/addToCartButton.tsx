7cc287011a72579796afc8077162878fc00142e9
Ida Casperson <ida_casperson@hotmail.com>
2024-03-18 12:45:38 +0100

    deleted more comments

--- app/ui/addToCartButton.tsx
+++ app/ui/addToCartButton.tsx
@@ -33,33 +33,3 @@ const AddToCartButton: React.FC<AddToCartButtonProps> = ({
 };
 
 export default AddToCartButton;
-
-// "use client";
-// import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
-// import { IconButton } from "@mui/material";
-// import React from "react";
-// import { Product } from "../../data/index";
-
-// interface AddToCartButtonProps {
-//   product: Product;
-//   title: string;
-//   // Lägg till funktion för att hantera "Lägg till i kundvagnen"
-//   handleAddToCart: (products: Product) => void;
-// }
-
-// const AddToCartButton: React.FC<AddToCartButtonProps> = ({
-//   product,
-//   handleAddToCart,
-// }) => {
-//   return (
-//     <IconButton
-//       aria-label="add to cart"
-//       // Anropa handleAddToCart-funktionen med produkttiteln
-//       onClick={() => handleAddToCart(product)}
-//     >
-//       <AddShoppingCartIcon />
-//     </IconButton>
-//   );
-// };
-
-// export default AddToCartButton;
