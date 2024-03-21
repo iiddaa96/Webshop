6e2d1b162eb126e6e8c640a1212e01f6ceaba790
Ida Casperson <ida_casperson@hotmail.com>
2024-03-21 15:50:20 +0100

    fixa nått konstigt error

--- app/ui/AddToCartButton.tsx
+++ app/ui/AddToCartButton.tsx
@@ -11,16 +11,19 @@ interface AddToCartButtonProps {
   // Lägg till funktion för att hantera "Lägg till i kundvagnen"
   handleAddToCart: (products: Product) => void;
 }
+
 /**
- * Komponent för en knapp för att lägga till en produkt i kundvagnen.
+ * Komponent för en knapp som lägger till en produkt i kundvagnen.
+ * Använder en ikon för att representera "Lägg till i kundvagnen".
  *
  * @component
- * @param {object} props - Props för komponenten.
+ * @param {Object} props - Egenskaper för komponenten.
  * @param {Product} props.product - Produktobjektet som ska läggas till i kundvagnen.
- * @param {string} props.title - Titeln för knappen (ej använd).
- * @param {Function} props.handleAddToCart - Funktion för att hantera lägg till i kundvagnen.
- * @returns {JSX.Element} Knapp för att lägga till i kundvagnen.
+ * @param {string} props.title - Titeln för produkten (ej använd).
+ * @param {Function} props.handleAddToCart - Funktion för att hantera "Lägg till i kundvagnen".
+ * @returns {JSX.Element} En ikonknapp för att lägga till produkten i kundvagnen.
  */
+
 const AddToCartButton: React.FC<AddToCartButtonProps> = ({
   product,
   handleAddToCart,
