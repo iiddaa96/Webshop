776403ca5e9e088103f8eff18ab73fb86e1d038a
Ida Casperson <ida_casperson@hotmail.com>
2024-05-16 16:03:53 +0200

    added bakgroundImage url

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,8 +12,11 @@ function Login() {
         justifyContent: "center",
         alignItems: "center",
         minHeight: "100vh",
-
+        backgroundImage:
+          "url('https://images.unsplash.com/photo-1517016006573-2eefaa2f5b63?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
         backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
       }}
     >
       <Grid container spacing={2} sx={{ maxWidth: 400, width: "100%", p: 2 }}>
@@ -23,7 +26,7 @@ function Login() {
               variant="body1"
               sx={{
                 fontSize: { xs: "25px", md: "3rem", lg: "2.6rem" },
-                fontFamily: "Caveat",
+                fontFamily: "times new roman",
               }}
             >
               Log In
@@ -62,6 +65,7 @@ function Login() {
                 mt: 2,
                 bgcolor: "#020202",
                 "&:hover": { bgcolor: "#38403a" },
+                fontFamily: "monospace",
               }}
             >
               Log In
