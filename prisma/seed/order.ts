c887d50d8232218860226f21041ad05ae55e8a61
ricoace10 <94618629+ricoace10@users.noreply.github.com>
2024-05-21 12:17:31 +0200

    bjdb

--- prisma/seed/order.ts
+++ prisma/seed/order.ts
@@ -3,25 +3,24 @@ import { db } from "../db";
 // Skapar default användare
 export async function order() {
   const test = await db.order.upsert({
-    where: {id: },
-    update: {
-    },
+    where: { id: 2 },
+    update: {},
     create: {
-        total: 500,
-        Order: [
-            {
-                title: "sås",
-                image: "link",
-                price: 5,
-                description: "test"
-            },
-            {
-                title: "sås 2",
-                image: "link",
-                price: 5,
-                description: "test"
-            },
-        ]
+      total: 500,
+      Order: [
+        {
+          title: "sås",
+          image: "link",
+          price: 5,
+          description: "test",
+        },
+        {
+          title: "sås 2",
+          image: "link",
+          price: 5,
+          description: "test",
+        },
+      ],
     },
   });
 }
