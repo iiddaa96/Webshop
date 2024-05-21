f5bfd8023356080470b5709c6aac77483a97b410
ricoace10 <94618629+ricoace10@users.noreply.github.com>
2024-05-21 12:01:30 +0200

    Adding orders to the data base

---
+++ prisma/seed/order.ts
@@ -0,0 +1,27 @@
+import { db } from "../db";
+
+// Skapar default användare
+export async function order() {
+  const test = await db.order.upsert({
+    where: {id: },
+    update: {
+    },
+    create: {
+        total: 500,
+        Order: [
+            {
+                title: "sås",
+                image: "link",
+                price: 5,
+                description: "test"
+            },
+            {
+                title: "sås 2",
+                image: "link",
+                price: 5,
+                description: "test"
+            },
+        ]
+    },
+  });
+}
--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,33 +1,13 @@
 import { db } from "../db";
 import { seedCatorgories as seedCatorgoriesWithProducts } from "./categoryWithProducts";
+import { order as seedOrder } from "./order";
 import { user as seedUser } from "./user";
-import { products } from "../products";
-import { PrismaClient } from "@prisma/client";
-
-//lägga Produckter I databas
-
-const prisma = new PrismaClient();
-async function add() {
-  for (let product of products) {
-    await prisma.product.create({
-      data: product,
-    });
-  }
-}
-
-add()
-  .catch((e) => {
-    console.log(e);
-    process.exit(1);
-  })
-  .finally(() => {
-    prisma.$disconnect();
-  });
 
 // Skapar default användare
 async function main() {
   await seedCatorgoriesWithProducts();
   await seedUser();
+  await seedOrder();
   // await seed other stuff
 }
 
