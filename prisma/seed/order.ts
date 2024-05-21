af247ab396e91d0be5016fd95658770aa86c600c
ricoace10 <94618629+ricoace10@users.noreply.github.com>
2024-05-21 15:56:20 +0200

    ndjs

--- prisma/seed/order.ts
+++ prisma/seed/order.ts
@@ -1,27 +1,27 @@
-import { number } from "zod";
 import { db } from "../db";
 
 // Skapar default användare
 export async function order() {
   const test = await db.order.upsert({
-    where: { id: 2 },
+    where: { id: 5 },
     update: {},
     create: {
       total: 500,
-      Order: [
-        {
-          title: "sås",
-          image: "link",
-          price: 5,
-          description: "test",
-        },
-        {
-          title: "sås 2",
-          image: "link",
-          price: 5,
-          description: "test",
-        },
-      ],
+
+      orderDetails: {
+        create: [
+          {
+            productId: 5,
+            quantity: 1,
+            subTotal: 7686,
+          },
+          {
+            productId: 5,
+            quantity: 1,
+            subTotal: 7686,
+          },
+        ],
+      },
     },
   });
 }
