83d8882eafce03362d53af0ad566f85836e413bb
Ida Casperson <ida_casperson@hotmail.com>
2024-05-26 17:41:42 +0200

    style cards so they are the same size

--- app/ui/CardsForHomepage/CardsHeader.tsx
+++ app/ui/CardsForHomepage/CardsHeader.tsx
@@ -4,16 +4,19 @@ import CardMedia from "@mui/material/CardMedia";
 import Link from "next/link";
 
 export default function ActionAreaCard() {
+  const cardStyle = { width: 250, height: 300 }; // Fasta dimensioner för kort
+  const mediaStyle = { height: 270 }; // Fasta dimensioner för bilder
+
   return (
     <Grid container spacing={2} justifyContent="center">
-      <Grid item>
-        <Card>
+      <Grid sx={{ marginBottom: "10px" }} item>
+        <Card sx={cardStyle}>
           <CardActionArea>
             <Link href="/categories/nyheter">
               <CardMedia
-                sx={{ maxWidth: 250 }}
+                sx={mediaStyle}
                 component="img"
-                image="https://plus.unsplash.com/premium_photo-1663133679087-bc5deb50ab00?q=80&w=1902&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                image="https://images.unsplash.com/photo-1575424909138-46b05e5919ec?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="green iguana"
               />
             </Link>
@@ -24,11 +27,11 @@ export default function ActionAreaCard() {
         </Card>
       </Grid>
       <Grid item>
-        <Card>
+        <Card sx={cardStyle}>
           <CardActionArea>
             <Link href="/categories/rea">
               <CardMedia
-                sx={{ maxWidth: 250 }}
+                sx={mediaStyle}
                 component="img"
                 image="https://plus.unsplash.com/premium_photo-1663133679087-bc5deb50ab00?q=80&w=1902&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="green iguana"
@@ -41,13 +44,13 @@ export default function ActionAreaCard() {
         </Card>
       </Grid>
       <Grid item>
-        <Card>
+        <Card sx={cardStyle}>
           <CardActionArea>
             <Link href="/categories/badleksaker">
               <CardMedia
-                sx={{ maxWidth: 250 }}
+                sx={mediaStyle}
                 component="img"
-                image="https://plus.unsplash.com/premium_photo-1663133679087-bc5deb50ab00?q=80&w=1902&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                image="https://images.unsplash.com/photo-1501426026826-31c667bdf23d?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="green iguana"
               />
             </Link>
@@ -62,13 +65,13 @@ export default function ActionAreaCard() {
         </Card>
       </Grid>
       <Grid item>
-        <Card>
+        <Card sx={cardStyle}>
           <CardActionArea>
             <Link href="/categories/handdukar">
               <CardMedia
-                sx={{ maxWidth: 250 }}
+                sx={mediaStyle}
                 component="img"
-                image="https://plus.unsplash.com/premium_photo-1663133679087-bc5deb50ab00?q=80&w=1902&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                image="https://plus.unsplash.com/premium_photo-1692439058306-c172c696d299?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="green iguana"
               />
             </Link>
