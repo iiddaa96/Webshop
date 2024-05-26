79cfa883708a742d9f907a3badff8fdb05c9716b
Jonatan Helander <Jonatanhelander@hotmail.com>
2024-05-26 14:12:16 +0200

    Går att toggla isSent värdet i order modellen, och omrenderas på knapptrycket

---
+++ app/admin/component/OrderList.tsx
@@ -0,0 +1,29 @@
+import { auth } from "@/app/auth";
+import { db } from "@/prisma/db";
+import { Box, List, ListItem } from "@mui/material";
+import ToggleIsSentButton from "./ToggleIsSentButton";
+
+export default async function OrderList() {
+  const session = await auth();
+  const orders = await db.order.findMany({});
+
+  return (
+    <List>
+      {orders.map((order) => (
+        <ul key={order.id}>
+          <ListItem>Total price: {order.total}kr</ListItem>
+          <ListItem>User ID: {order.userId}</ListItem>
+          <ListItem>Date: {order.createdAt.toString()}</ListItem>
+          <Box
+            sx={{
+              display: "flex",
+            }}
+          >
+            <ListItem>{order.isSent.toString()}</ListItem>
+            <ToggleIsSentButton orderId={order.id} />
+          </Box>
+        </ul>
+      ))}
+    </List>
+  );
+}
