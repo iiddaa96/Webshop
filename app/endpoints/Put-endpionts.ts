ad2cb7306e3bee8813f7d5894ae887aa4b4fc062
ricoace10 <94618629+ricoace10@users.noreply.github.com>
2024-05-27 15:17:13 +0200

    Final Put end points commit

--- app/endpoints/Put-endpionts.ts
+++ app/endpoints/Put-endpionts.ts
@@ -1,31 +1,22 @@
-"use server";
+("use server");
 
 import { db } from "@/prisma/db";
 import { Product } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
+export async function update(request: NextRequest) {
   const products = await db.product.findMany();
   return NextResponse.json(products, { status: 200 });
 }
 
-export async function PUT(request: NextRequest) {
-  const { id } = request.nextUrl.searchParams;
-  const updatedProduct: Product = await request.json();
-
-  try {
-    const product = await db.product.update({
-      where: { id: parseInt(id) },
-      data: {
-        title: updatedProduct.title,
-        image: updatedProduct.image,
-        price: updatedProduct.price,
-        description: updatedProduct.description,
-      },
-    });
-
-    return NextResponse.json(product, { status: 200 });
-  } catch (error) {
-    return NextResponse.json({ message: error.message }, { status: 400 });
-  }
+export async function editProduct(updatedProduct: Product) {
+  return await db.product.update({
+    where: { id: updatedProduct.id },
+    data: {
+      title: updatedProduct.title,
+      image: updatedProduct.image,
+      price: updatedProduct.price,
+      description: updatedProduct.description,
+    },
+  });
 }
