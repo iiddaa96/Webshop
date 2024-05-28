304e6e7fdd6f34dfc1e985046017f4fb3ffaf895
ricoace10 <94618629+ricoace10@users.noreply.github.com>
2024-05-28 11:13:48 +0200

    comiits

--- app/endpoints/duplicate-endpionts.ts
+++ app/endpoints/duplicate-endpionts.ts
@@ -1,25 +1,6 @@
-("use server");
+"use server";
 
 import { db } from "@/prisma/db";
-import { Product } from "@prisma/client";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function update(request: NextRequest) {
-  const products = await db.product.findMany();
-  return NextResponse.json(products, { status: 200 });
-}
-
-export async function editProduct(updatedProduct: Product) {
-  return await db.product.update({
-    where: { id: updatedProduct.id },
-    data: {
-      title: updatedProduct.title,
-      image: updatedProduct.image,
-      price: updatedProduct.price,
-      description: updatedProduct.description,
-    },
-  });
-}
 
 export async function duplicateProduct(productId: number) {
   const product = await db.product.findUnique({
