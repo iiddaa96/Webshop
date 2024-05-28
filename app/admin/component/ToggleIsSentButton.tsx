b4b9e8b8b9a2755fadba2b3180aba194e58f80f4
Ida Casperson <ida_casperson@hotmail.com>
2024-05-28 10:13:06 +0200

    added little styleing to orders in admin

--- app/admin/component/ToggleIsSentButton.tsx
+++ app/admin/component/ToggleIsSentButton.tsx
@@ -5,5 +5,22 @@ export default function ToggleIsSentButton({ orderId }: { orderId: number }) {
   const toggleIsSent = () => {
     isSentEndpoint(orderId);
   };
-  return <button onClick={toggleIsSent}>Toggle</button>;
+  return (
+    <button
+      style={{
+        backgroundColor: "#363635",
+        color: "#FFFFFF",
+        border: "none",
+        borderRadius: "10px",
+        padding: "10px 20px",
+        fontSize: "12px",
+        cursor: "pointer",
+        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+        transition: "background-color 0.3s ease",
+      }}
+      onClick={toggleIsSent}
+    >
+      Toggle
+    </button>
+  );
 }
