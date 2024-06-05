8fb8d3864417a156fc48566c9fb0104a90df4877
ricoace10 <94618629+ricoace10@users.noreply.github.com>
2024-06-05 09:39:01 +0200

    commits

---
+++ app/checkout/Checkout.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { Box, Container, Grid, Typography } from "@mui/material";
+import { useCart } from "../context/CartContext";
+import { BackButton } from "../ui/BackButton";
+import PaymentSection from "../ui/PaymentSection";
+import { CartItems } from "./components/CartItems";
+import TotalPrice from "./components/TotalPrice";
+
+const Checkout = () => {
+  const { cart } = useCart();
+
+  return (
+    <Container maxWidth="md" component="main">
+      <BackButton />
+      <Typography
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          fontWeight: "bold",
+          fontSize: "30px",
+          marginBottom: "20px",
+        }}
+      >
+        Kassa
+      </Typography>
+      <Grid container spacing={3}>
+        <CartItems cart={cart} />
+      </Grid>
+      <Grid container sx={{ alignItems: "center", marginTop: "20px" }}>
+        <Grid item xs={6}>
+          <Box>
+            <Typography variant="h6">Totalt:</Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+          <Box sx={{ textAlign: "right" }}>
+            <Typography
+              variant="body1"
+              sx={{
+                fontWeight: "bold",
+                display: "inline-block",
+                verticalAlign: "middle",
+              }}
+            >
+              <TotalPrice cart={cart} />
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+      <PaymentSection />
+    </Container>
+  );
+};
+
+export default Checkout;
