cd1ee903da039f170227c0b82b3c360bec5dec31
sallymaria99 <144012169+sallymaria99@users.noreply.github.com>
2024-06-06 16:28:44 +0200

    bilder ligger rätt på singelProductPage

--- app/ui/SingelProduct.tsx
+++ app/ui/SingelProduct.tsx
@@ -12,12 +12,20 @@ export default function SingleProduct({ product }: Props) {
   return (
     <main>
       <Grid container spacing={2}>
-        <Grid item xs={10} sm={6}>
+        <Grid
+          item
+          xs={10}
+          sm={6}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
           <Box
             sx={{
               flexGrow: 1,
               marginLeft: "5.3rem",
-              marginBottom: "-4rem",
+              marginBottom: { xs: "-4rem", sm: "-4rem", md: "4rem" },
               marginTop: "10px",
             }}
             style={{ filter: isOutOfStock ? "grayscale(100%)" : "none" }}
@@ -27,14 +35,25 @@ export default function SingleProduct({ product }: Props) {
                 component="img"
                 src={product.image}
                 alt={product.title}
-                style={{ maxWidth: "100%" }}
+                sx={{
+                  maxWidth: "100%",
+                  height: "auto",
+                  maxHeight: {
+                    xs: "auto",
+                    sm: "300px",
+                    md: "400px",
+                    lg: "500px",
+                    xl: "600px",
+                  },
+                  objectFit: "contain",
+                }}
               />
             </div>
           </Box>
         </Grid>
         <Grid item xs={12} sm={6}>
           <Box
-            sx={{ flexGrow: 1, padding: "80px 30px" }}
+            sx={{ flexGrow: 1, padding: "70px 10px" }}
             style={{ color: isOutOfStock ? "gray" : "inherit" }}
           >
             <Typography variant="h4" gutterBottom>
